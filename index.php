4b0296602e31ec02d6ad212839181932c59d45e1
Mirelly <mirellyagv@gmail.com>
2019-09-27 14:27:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-<!-- 	<title>Hola mundo</title>
+	<title>Hola mundo</title>
 </head>
 <body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-	<h5>HOLA MUNDO USANDO REPOSITORIOS</h5> -->
+	<h5>HOLA MUNDO USANDO REPOSITORIOS</h5>
 
 	prueba de edicion desde sublime de pc
-=======
-	<h5>HOLA MUNDO USANDO REPOSITORIOS</h5>
->>>>>>> parent of 2c09b6f... Update index.php
-=======
-	<h5>HOLA MUNDO USANDO REPOSITORIOS</h5>
->>>>>>> parent of 2c09b6f... Update index.php
+
+
+
+ <p style="color:blue">prueba de ramas</p>
 </body>
 </html>
