ef02cb8089e2a3cc418ac3054ea20a22dd0beb46
Mirelly <mirellyagv@gmail.com>
2019-09-27 12:26:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
 <!-- 	<title>Hola mundo</title>
 </head>
 <body>
+<<<<<<< HEAD
 	<h5>HOLA MUNDO USANDO REPOSITORIOS</h5> -->
 
 	prueba de edicion desde sublime de pc
+=======
+	<h5>HOLA MUNDO USANDO REPOSITORIOS</h5>
+>>>>>>> parent of 2c09b6f... Update index.php
 </body>
 </html>
