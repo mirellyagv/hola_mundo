ad60120e430693f072da42a590fc5b91dcb83850
jhonamg <orion01123581321@gmail.com>
2019-09-30 17:57:45 -0500

    Update index.php
    Introduje un canvas.
    con la descripcion "hola mundo"

--- index.php
+++ index.php
@@ -11,5 +11,21 @@
 =======
 	<h5>HOLA MUNDO USANDO REPOSITORIOS</h5>
 >>>>>>> parent of 2c09b6f... Update index.php
+
+<br/>
+<br/>
+
+<!-- insercion de otra wada -->
+<canvas id="myCanvas" width="400" height="100" style="border:1px solid #d3d3d3;">
+Your browser does not support the HTML5 canvas tag.</canvas>
+
+<script>
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+ctx.strokeText("Hola Mundo Mundial!! n.n",10,50);
+</script>
+
+
 </body>
 </html>
